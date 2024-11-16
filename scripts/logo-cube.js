4806e523cb1eb4626cdcb19b9dfba15479da4d79
MaxMavr <maxmavr2005@gmail.com>
2024-11-16 22:35:58 +0300

    add vendor prefixes to grids

--- scripts/logo-cube.js
+++ scripts/logo-cube.js
@@ -26,7 +26,14 @@ function updateCubeRotation(event) {
     const angleY = Math.atan((clientX - (clientWidth / 2 + offsetLeft)) / (2.5 * clientHeight));
 
     cube.style.transition = transitionFast;
-    cube.style.transform = `rotateX(${-rad2deg * angleX}deg) rotateY(${rad2deg * angleY}deg)`;
+    cube.style.webkitTransition = transitionFast;
+    cube.style.mozTransition = transitionFast;
+    cube.style.msTransition = transitionFast;
+
+    const transformValue = `rotateX(${-rad2deg * angleX}deg) rotateY(${rad2deg * angleY}deg)`;
+    cube.style.transform = transformValue;
+    cube.style.WebkitTransform = transformValue;
+    cube.style.MozTransform = transformValue;
   }, 16);
 }
 
@@ -34,12 +41,20 @@ function resetCubeRotation() {
   clearTimeout(debounceTimeout);
   
   cube.style.transition = transitionSlow;
-  cube.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  cube.style.webkitTransition = transitionSlow;
+  cube.style.mozTransition = transitionSlow;
+  cube.style.msTransition = transitionSlow;
+
+  const transformValue = 'rotateX(0deg) rotateY(0deg)';
+  cube.style.transform = transformValue;
+  cube.style.WebkitTransform = transformValue;
+  cube.style.MozTransform = transformValue;
 }
 
 function updateCubeParameters() {
   const cubeOrigin = `${logo.clientHeight / 2}px ${logo.clientHeight / 2}px ${logo.clientHeight / -2}px`;
   logo.style.perspective = `${2 * logo.clientHeight}px`;
+  logo.style.webkitPerspective = `${2 * logo.clientHeight}px`;
   document.documentElement.style.setProperty('--cube-origin', cubeOrigin);
 }
 
